d716a948f6f16741d05a4050f6472ce20be10aee
Jeff Lau <jeff-lau@live.com>
2016-07-15 23:29:46 +0800

    Added main images to singleGym page

--- app/components/gym.js
+++ app/components/gym.js
@@ -2,19 +2,30 @@ import React from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  Image,
+  StyleSheet,
+  Dimensions
 } from 'react-native';
 import { connect } from 'react-redux';
+import getMainImage from '../util/getGymImages';
 
-const Gym = () => (
+const { height, width } = Dimensions.get('window');
+
+const Gym = ({ gym }) => (
   <View style={styles.container}>
-    <Text>Gym View</Text>
+    <Image source={getMainImage(gym.slug)} style={styles.mainImage}/>
+    <Text>{gym.description}</Text>
   </View>
 )
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 64,
     flex: 1
+  },
+  mainImage: {
+    width: width,
+    height: height/3
   }
 });
 
